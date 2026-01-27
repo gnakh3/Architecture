ee48fd7250bf91f7cb858a603cf1d33d691049c8
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2026-01-27 20:11:21 +0400

    Imported home page

--- architect/app/page.tsx
+++ architect/app/page.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Home from "./components/organisms/Home/Home"
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Home />
+    </div>
   )
 }
 
