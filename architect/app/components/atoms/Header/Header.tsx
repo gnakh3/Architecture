2f952a7c34c96b6d8f79f4a3acb4e345d861cba9
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2026-01-27 20:24:07 +0400

    Header Done

--- architect/app/components/atoms/Header/Header.tsx
+++ architect/app/components/atoms/Header/Header.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Image from "next/image"
 
 const header = () => {
   return (
-    <div>header</div>
+    <div className="flex items-center justify-between w-[504px] h-[40px] pt-[56px]">
+        <Image src="/logo.svg" alt="Logo" width={100} height={100} />
+        <div className="font-bold text-[18px] leading-[25px] text-[#7D828F] flex items-center gap-[51px]">
+          <div>Portfolio</div>
+          <div>About Us</div>
+          <div>Contact</div>
+        </div>
+    </div>
   )
 }
 
