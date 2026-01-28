5806dc6127ad8c57d7a1a65561c3312987ff3fef
bagashvili <113350170+Tamari13@users.noreply.github.com>
2026-01-28 08:36:03 +0400

    KAN-6

--- architect/app/components/molecules/ArchStuido/ArchStudio.tsx
+++ architect/app/components/molecules/ArchStuido/ArchStudio.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import Bitmap from '@/public/Bitmap.svg';
 
 const ArchStudio = () => {
   return (
-    <div>ArchStudio</div>
+    <div className="pt-10 pb-10 flex flex-col justify-center items-center">
+      <div className="w-[75%] h-140 flex flex-col relative" 
+        style={{backgroundImage: `url(${Bitmap.src})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat"}} >
+        <div className="w-136.25 pl-32.5 pt-41.25 flex flex-col justify-center gap-6.25">
+          <h1 className="font-bold text-[72px] leading-16 tracking-[-2px] text-white" style={{ fontFamily: 'League Spartan' }}>
+            Small team, big ideas
+          </h1>
+          <button className="flex justify-center items-center bg-[#1B1D23] w-46.75 h-18 font-bold text-[18px] leading-6.25 text-right text-white hover:bg-[#60636D] hover:cursor-pointer"
+            style={{ fontFamily: 'League Spartan' }}>
+            About Us <span className="pl-5 font-bold text-[18px] leading-6.25" style={{ fontFamily: 'League Spartan' }}>→</span>
+          </button>
+        </div>  
+      </div>
+    </div>
   )
 }
 
