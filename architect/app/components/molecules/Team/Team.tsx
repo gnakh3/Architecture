5adcda34ccb042d2e0cd054631dc14c7039b9407
bagashvili <113350170+Tamari13@users.noreply.github.com>
2026-01-30 17:11:50 +0400

    fix/KAN-9-and-KAN-6-button-correct

--- architect/app/components/molecules/Team/Team.tsx
+++ architect/app/components/molecules/Team/Team.tsx
@@ -11,9 +11,10 @@ const Team = () => {
           <h1 className="font-bold text-[72px] leading-16 tracking-[-2px] text-white" style={{ fontFamily: 'League Spartan' }}>
             Small team, big ideas
           </h1>
-          <button className="w-46.75 h-18 flex items-center justify-center bg-[#1B1D23] px-8 py-5 text-lg font-bold text-white transition-colors hover:bg-[#60636D] cursor-pointer"
+          <button className="w-46.75 h-18 flex gap-3 items-center justify-center bg-[#1B1D23] px-8 py-5 text-lg font-bold text-white  hover:bg-[#60636D] hover:scale-105  cursor-pointer transition-all duration-200 ease-in-out"
             style={{ fontFamily: 'League Spartan' }}>
-            About Us <span className="pl-5 font-bold text-[18px] leading-6.25" style={{ fontFamily: 'League Spartan' }}>→</span>
+            About Us 
+            <Image src="/Arrow.svg" alt="Arrow" width={16} height={16}  /> 
           </button>
         </div>  
       </div>
