ede5a1e491c670d67a7e9c2272bae228a05012f3
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2026-01-31 23:16:15 +0400

    Increase Home component height from 350vh to 400vh

--- architect/app/components/organisms/Home/Home.tsx
+++ architect/app/components/organisms/Home/Home.tsx
@@ -9,7 +9,7 @@ import Footer from "../../atoms/Footer/Footer"
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-between bg-white w-full h-[350vh] pl-[165px] pr-[165px]">
+    <div className="flex flex-col justify-between bg-white w-full h-[400vh] pl-[165px] pr-[165px]">
       <Header />
       <Welcome />
       <ArchStudio />
