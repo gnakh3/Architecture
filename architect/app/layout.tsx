4941079697e5c38c4ec0f8d818d377011e7a7004
Gigi Nakhutsrishvili <nakhutsrishviligigi@gmail.com>
2026-02-06 21:34:39 +0400

    Imported font in layout

--- architect/app/layout.tsx
+++ architect/app/layout.tsx
@@ -1,18 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { League_Spartan } from 'next/font/google'
+
 import "./globals.css";
 import Header from "@/components/atoms/Header/Header";
 import Footer from "@/components/atoms/Footer/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const geistSans = League_Spartan({
+  variable: "--font-league-spartan",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased pr-[165px] pl-[165px]`}
+        className={`${geistSans.variable} antialiased pr-[165px] pl-[165px]`}
       >
         <Header/>
         {children}
